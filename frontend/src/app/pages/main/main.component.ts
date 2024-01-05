571ac14b0529eb004211583fd9312ad2d3dc71df
Natanael Neto <nesn.gamedev@gmail.com>
2024-01-05 10:31:39 -0300

    C1 - Criação de estilos e páginas

---
+++ frontend/src/app/pages/main/main.component.ts
@@ -0,0 +1,9 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.scss'],
+})
+export class MainComponent { }
