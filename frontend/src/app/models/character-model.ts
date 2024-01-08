7da945fcae7b7ddfccdccd632a7b2c9b2b8ae4f2
Natanael Neto <nesn.gamedev@gmail.com>
2024-01-08 15:10:10 -0300

    C4 - Header da página Session

---
+++ frontend/src/app/models/character-model.ts
@@ -0,0 +1,40 @@
+export class CharacterModel {
+  // Atributos Pessoais
+  name: string;
+  race: string;
+  class: string;
+  level: number;
+
+  // Contadores
+  life: number;
+  mana: number;
+
+  // Atributos
+  strength: number;
+  agility: number;
+  intelligence: number;
+  will: number;
+
+  equipments: [];
+  abilities: [];
+
+  block: {
+    armorBonus: 0;
+    blockBonus: 0;
+  }
+
+  dodge: {
+    armorBonus: 0;
+    blockBonus: 0;
+  }
+
+  getBlock(): number {
+    return 5 + this.strength + this.block.armorBonus + this.block.blockBonus;
+  }
+
+  getDodge(): number {
+    return 5 + this.agility + this.dodge.armorBonus + this.dodge.blockBonus;
+  }
+
+  location?: string;
+}
