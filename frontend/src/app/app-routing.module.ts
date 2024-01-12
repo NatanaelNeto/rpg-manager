7262bc7b5d9aa1dfe2d841bc7a3320d28c7526c3
Natanael Neto <natanael.eneas2018@hotmail.com>
2024-01-11 22:39:41 -0300

    C7 - Adiciona nova página - base de dados

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './pages/main/main.component';
 import { SessionComponent } from './pages/session/session.component';
+import { DatabaseComponent } from './pages/database/database.component';
 
 const routes: Routes = [
   {
     path: '', component: MainComponent, children: [
-      { path: 'session', component: SessionComponent }
+      { path: 'session', component: SessionComponent },
+      { path: 'database', component: DatabaseComponent },
     ]
   },
 ];
