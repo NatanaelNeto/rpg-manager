53ae4551c2f71bd2c829a158410e9335baafdf33
Natanael Neto <natanael.eneas2018@hotmail.com>
2024-01-17 21:50:39 -0300

    25 - Aba de sessão - parte 2

--- frontend/src/app/pages/session/session.component.ts
+++ frontend/src/app/pages/session/session.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { CharacterModel } from 'src/app/models/character-model';
+import { DatabaseService } from '../database/database.service';
+import { AlertService } from 'src/app/components/alert/alert.service';
 
 @Component({
   selector: 'app-session',
@@ -41,6 +43,14 @@ export class SessionComponent {
     openMenu: false
   }
 
+  chars: CharacterModel[] = [];
+  enemies: CharacterModel[] = [];
+
+  constructor(private db: DatabaseService, private alert: AlertService) {
+    this.db.getAllCharacters().subscribe(c => this.chars = c);
+    this.db.getAllEnemies().subscribe(e => this.enemies = e);
+  }
+
   setMainChar(c: CharacterModel) {
     this.char = c;
   }
@@ -51,5 +61,39 @@ export class SessionComponent {
     else if (i <= 5) return 'yellow';
     else if (i <= 10) return 'green';
     return 'blue';
+  }
+
+  setLife(value: number) {
+    if(this.char.currLife + value < 0) {
+      this.char.currLife = 0;
+    }
+    else if(this.char.currLife + value > this.char.life) {
+      this.char.currLife = this.char.life;
+    }
+    else {
+      this.char.currLife += value;
+    }
+    this.updateDB();
+  }
+
+  setMana(value: number) {
+    if(this.char.currMana + value < 0) {
+      this.char.currMana = 0;
+    }
+    else if(this.char.currMana + value > this.char.mana) {
+      this.char.currMana = this.char.mana;
+    }
+    else {
+      this.char.currMana += value;
+    }
+    this.updateDB();
+  }
+
+  updateDB() {
+    this.db.updateChar(this.char, this.chars.some(c => c.name == this.char.name)).subscribe(() => {
+      this.alert.openPanel({state: true, type: 'sucess', message: 'Atualizado!'});
+      this.db.getAllCharacters().subscribe(c => this.chars = c);
+    this.db.getAllEnemies().subscribe(e => this.enemies = e);
+    });
   }
 }
